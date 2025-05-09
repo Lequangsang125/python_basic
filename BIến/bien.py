124c2f377712c30e62965434cccdd401ba828d30
Le Quang Sang <sanglq125@gmail.com>
2025-05-09 20:45:40 +0700

    first commit

---
+++ BIến/bien.py
@@ -0,0 +1,46 @@
+# là nơi chứa dữ liệu
+# python không có lệnh để khai báo
+# đc tạo ra khi gán giá trị cho nó
+# biến không cần khai báo kiểu dữ liệu và có thể gán lại kiểu dữ liệu
+# tên biến phân biệt chữ hoa và thường
+x = 4
+x = 'hello'
+print(x)
+
+#Ép kiểu
+x = str(3)
+print(x)
+y = int(3)
+print(y)
+z = float(3)
+print(z)
+
+# kiểm tra kiểu dữ liệu 
+print(type(x))
+print(type(y))
+print(type(z))
+
+### Tên biến
+# Tên biến phải bắt đầu chữ cái hoặc ký tự gạch dưới
+# Tên biến không thể bắt đầu bằng số
+# Tên biến phân biệt chữ hoa và thường
+
+# Biến python - gán nhiều giá trị
+x, y, z = "Orange","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+
+# Gán 1 giá trị cho nhiều biến 
+x = y = z = 'orange'
+print(x)
+print(y)
+print(z)
+
+# Giải nén trong python
+# Biến đầu ra 
+x = 'l'
+y = 'q'
+z = 's'
+print(x,y,z)
+print(x + y + z)
---
+++ cuphap.py
@@ -0,0 +1,12 @@
+# THỤT LỀ : trong python thụt lề là 1 khối mã
+# Phổ biến là 4 khoảng trắng, tối thiểu có 1 
+# trong cùng một khối mã phải có khoảng trắng bằng nhau
+
+# if 5>2 : 
+#     print('5 lớn hơn 2')
+#     print('5 lon hon 3')
+
+## BIẾN
+# các biến đc tạo ra khi gán giá trị cho nó
+x = 5 
+print(x)
